764a31faba06c2d7d97f55ca5f2652438b83ad53
Hugo Parente Lima <hugo.lima@openbossa.org>
2013-06-10 19:47:52 -0300

    Fix WTF PlatformNix.cmake
    - Some cpp files now exists on CMakeLists.txt
    - There's no sense in including JSC dirs on WTF include directories.
    - CMAKE_DL_LIBS is already on CMakeLists.txt
    
    Reviewed by: Lauro Neto

--- Source/WTF/wtf/PlatformNix.cmake
+++ Source/WTF/wtf/PlatformNix.cmake
@@ -3,22 +3,14 @@ list(APPEND WTF_SOURCES
     gobject/GOwnPtr.cpp
     gobject/GRefPtr.cpp
     gobject/GlibUtilities.cpp
-
-    OSAllocatorPosix.cpp
-    ThreadIdentifierDataPthreads.cpp
-    ThreadingPthreads.cpp
-
 )
 
 list(APPEND WTF_LIBRARIES
     pthread
     ${GLIB_LIBRARIES}
     ${GLIB_GIO_LIBRARIES}
-    ${CMAKE_DL_LIBS}
 )
 
 list(APPEND WTF_INCLUDE_DIRECTORIES
     ${GLIB_INCLUDE_DIRS}
-    ${JAVASCRIPTCORE_DIR}/wtf/gobject
-    ${JAVASCRIPTCORE_DIR}/wtf/unicode/
 )
