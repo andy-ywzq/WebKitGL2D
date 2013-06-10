01d22d691bcadb0aa0e4791e742c78d717d33b89
Hugo Parente Lima <hugo.lima@openbossa.org>
2013-06-10 19:22:11 -0300

    Remove unneeded PlatformNix.cmake file on JSC.
    Reviewed by: Lauro Neto

--- Source/JavaScriptCore/PlatformNix.cmake
+++
@@ -1,8 +0,0 @@
-list(APPEND JavaScriptCore_SOURCES
-    jit/ExecutableAllocatorFixedVMPool.cpp
-    jit/ExecutableAllocator.cpp
-)
-
-list(APPEND JavaScriptCore_INCLUDE_DIRECTORIES
-    ${WTF_DIR}/wtf/gobject
-)
