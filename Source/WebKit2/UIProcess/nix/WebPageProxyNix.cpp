andy-ywzq/WebKitGL2D

commit 5f91be37af9b9245f9d6af63989bb36a7626946d

Respect WKPageSetApplicationNameForUserAgent and use the string on the generated user agent.
Reviewed by: Luciano Wolf

Source/WebKit2/UIProcess/nix/WebPageProxyNix.cpp
@@ -37,7 +37,7 @@
 
 namespace WebKit {
 
-String WebPageProxy::standardUserAgent(const String&)
+String WebPageProxy::standardUserAgent(const String& applicationName)
 {
     WTF::String platform;
     WTF::String version;
@@ -49,14 +49,14 @@ String WebPageProxy::standardUserAgent(const String&)
     platform = "Unknown";
 #endif
 
-    version = String::number(WEBKIT_MAJOR_VERSION) + '.' + String::number(WEBKIT_MINOR_VERSION) + '+';
+    version = String::number(WEBKIT_MAJOR_VERSION) + '.' + String::number(WEBKIT_MINOR_VERSION) + ' ';
     struct utsname name;
     if (uname(&name) != -1)
         osVersion = WTF::String(name.sysname) + " " + WTF::String(name.machine);
     else
         osVersion = "Unknown";
 
-    return "Mozilla/5.0 (" + platform + "; " + osVersion + ") AppleWebKit/" + version + " (KHTML, like Gecko) Version/5.0 Safari/" + version;
+    return "Mozilla/5.0 (" + platform + "; " + osVersion + ") AppleWebKit/" + version + " (KHTML, like Gecko) " + applicationName + " Safari/" + version;
 }
 
 void WebPageProxy::getEditorCommandsForKeyEvent(Vector<WTF::String>&)

commit 902787302273caf5835f3e0dd1bbf65bf6038fe2

Fix regression about user agent string caused by 5f91be37af9b9245f9d6af63989bb36a7626946d.
Reviewed by: Marcelo Lira

Source/WebKit2/UIProcess/nix/WebPageProxyNix.cpp
@@ -34,6 +34,7 @@
 #include "WebKitVersion.h"
 
 #include <sys/utsname.h>
+#include <wtf/text/StringBuilder.h>
 
 namespace WebKit {
 
@@ -49,14 +50,29 @@ String WebPageProxy::standardUserAgent(const String& applicationName)
     platform = "Unknown";
 #endif
 
-    version = String::number(WEBKIT_MAJOR_VERSION) + '.' + String::number(WEBKIT_MINOR_VERSION) + ' ';
+    version = String::number(WEBKIT_MAJOR_VERSION) + '.' + String::number(WEBKIT_MINOR_VERSION);
     struct utsname name;
     if (uname(&name) != -1)
         osVersion = WTF::String(name.sysname) + " " + WTF::String(name.machine);
     else
         osVersion = "Unknown";
 
-    return "Mozilla/5.0 (" + platform + "; " + osVersion + ") AppleWebKit/" + version + " (KHTML, like Gecko) " + applicationName + " Safari/" + version;
+    WTF::StringBuilder userAgent;
+    userAgent.append("Mozilla/5.0 (");
+    userAgent.append(platform);
+    userAgent.append("; ");
+    userAgent.append(osVersion);
+    userAgent.append(") AppleWebKit/");
+    userAgent.append(version);
+    userAgent.append(" (KHTML, like Gecko)");
+    if (!applicationName.isEmpty()) {
+        userAgent.append(" ");
+        userAgent.append(applicationName);
+    }
+    userAgent.append(" Safari/");
+    userAgent.append(version);
+
+    return userAgent.toString();
 }
 
 void WebPageProxy::getEditorCommandsForKeyEvent(Vector<WTF::String>&)