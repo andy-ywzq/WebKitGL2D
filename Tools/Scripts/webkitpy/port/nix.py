andy-ywzq/WebKitGL2D

commit 61b798e0ac1711f940b01ba4231d555b1ed77382

Remove the run-with-jhbuild script.
The only reference for this script was on port.py and it's uneeded there.

Reviewed by: Thiago Lacerda

Tools/Scripts/webkitpy/port/nix.py
@@ -52,7 +52,7 @@ class NixPort(Port):
     def __init__(self, *args, **kwargs):
         super(NixPort, self).__init__(*args, **kwargs)
 
-        self._jhbuild_wrapper_path = self.path_from_webkit_base('Tools', 'nix', 'run-with-jhbuild')
+        self._jhbuild_wrapper_path = [self.path_from_webkit_base('Tools', 'jhbuild', 'jhbuild-wrapper'), '--nix', 'run']
 
         self.set_option_default('wrapper', self._jhbuild_wrapper_path)
         self.set_option_default('webkit_test_runner', True)

Tools/nix/run-with-jhbuild
@@ -1,26 +1,0 @@
-#!/usr/bin/env python
-# Copyright (C) 2011 Igalia S.L.
-# Copyright (C) 2012 Intel Corporation
-# Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies).
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-
-import common
-import os
-import sys
-
-
-jhbuild_wrapper = common.top_level_path('Tools', 'jhbuild', 'jhbuild-wrapper')
-os.execve(jhbuild_wrapper, [jhbuild_wrapper, '--nix', 'run'] + sys.argv[1:], os.environ)

commit 1d80b4cd0270f7642cbd0990595cdd5879d6ac6b

Fix webkitpy tests failure caused by commit 61b798e0ac1711f940b01ba4231d555b1ed77382
Reviewed by: Trust me

Tools/Scripts/webkitpy/port/nix.py
@@ -54,7 +54,7 @@ class NixPort(Port):
 
         self._jhbuild_wrapper_path = [self.path_from_webkit_base('Tools', 'jhbuild', 'jhbuild-wrapper'), '--nix', 'run']
 
-        self.set_option_default('wrapper', self._jhbuild_wrapper_path)
+        self.set_option_default('wrapper', ' '.join(self._jhbuild_wrapper_path))
         self.set_option_default('webkit_test_runner', True)
         self.webprocess_cmd_prefix = self.get_option('webprocess_cmd_prefix')
 
@@ -100,7 +100,7 @@ class NixPort(Port):
         return self._build_path('bin', 'ImageDiff')
 
     def _image_diff_command(self, *args, **kwargs):
-        return [self._jhbuild_wrapper_path] + super(NixPort, self)._image_diff_command(*args, **kwargs)
+        return self._jhbuild_wrapper_path + super(NixPort, self)._image_diff_command(*args, **kwargs)
 
     def _search_paths(self):
         return [self.port_name]